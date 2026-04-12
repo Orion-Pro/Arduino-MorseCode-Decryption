fec3080dc6ce78ded4264138bd9a841a4cdbeb75
Orion-Pro <jules.wygas@gmail.com>
2026-04-11 22:22:59 -0400

    Used chatGPT to generate the constructor for the alphabet tree. I've also been using it as a syntax reviewer. I've only used Java for the past 4 months, I needed to remind myself of the differences between the languages. Did a complete logical overhaul, changed the data structure, decode method. More details in the ReadME

--- Arduino-MorseCode-Decryption/Morse_Code_Reader/Morse_Code_Reader.ino
+++ Arduino-MorseCode-Decryption/Morse_Code_Reader/Morse_Code_Reader.ino
@@ -1,5 +1,3 @@
-#include "UnorderedMap.h"
-#include "Hashtable.h"
 
 //Button pin #
 const byte button = 8;
@@ -15,142 +13,149 @@ int press_duration = 0;
 int pause_duration = 0;
 
 //Might be useful for cleaner code;
-const int dot = 100; //We can give +/- 50 to begin with
-const int dash = 500; //where dash == 3*dot (again +/-50)
-const int space = 1100; //where space == 7*dot (+/-50)
+const char dot = 'd';       //100; //We can give +/- 50 to begin with //denoted d
+const char dash = 'D';      //500; //where dash == 3*dot (again +/-50) // denoted D
+const char space = 's';     //1100; //where space == 7*dot (+/-50) // denoted s
 
 //Initializes the condition for the state change loop
 byte previous_value = LOW;
 
-//--------------------------------------------------------------------------------------------------------------------------------
 
-int aSeq[] = {dot, dash};
-int bSeq[] = {dash, dot, dot, dot};
-int cSeq[] = {dash, dot, dash, dot};
-int dSeq[] = {dash, dot, dot};
-int eSeq[] = {dot};
-int fSeq[] = {dot, dot, dash, dot};
-int gSeq[] = {dash, dash, dot};
-int hSeq[] = {dot, dot, dot, dot};
-int iSeq[] = {dot, dot};
-int jSeq[] = {dot, dash, dash, dash};
-int kSeq[] = {dash, dot, dash};
-int lSeq[] = {dot, dash, dot, dot};
-int mSeq[] = {dash, dash};
-int nSeq[] = {dash, dot};
-int oSeq[] = {dash, dash, dash};
-int pSeq[] = {dot, dash, dash, dot};
-int qSeq[] = {dash, dash, dot, dash};
-int rSeq[] = {dot, dash, dot};
-int sSeq[] = {dot, dot, dot};
-int tSeq[] = {dash};
-int uSeq[] = {dot, dot, dash};
-int vSeq[] = {dot, dot, dot, dash};
-int wSeq[] = {dot, dash, dash};
-int xSeq[] = {dash, dot, dot, dash};
-int ySeq[] = {dash, dot, dash, dash};
-int zSeq[] = {dash, dash, dot, dot};
-
-// int oneSeq[]  = {dot, dash, dash, dash, dash};
-// int twoSeq[]  = {dot, dot, dash, dash, dash};
-// int threeSeq[]= {dot, dot, dot, dash, dash};
-// int fourSeq[] = {dot, dot, dot, dot, dash};
-// int fiveSeq[] = {dot, dot, dot, dot, dot};
-// int sixSeq[]  = {dash, dot, dot, dot, dot};
-// int sevenSeq[]= {dash, dash, dot, dot, dot};
-// int eightSeq[]= {dash, dash, dash, dot, dot};
-// int nineSeq[] = {dash, dash, dash, dash, dot};
-// int zeroSeq[] = {dash, dash, dash, dash, dash};
-
-// int questionSeq[] = {dot, dot, dash, dash, dot, dot};     
-// int exclamSeq[]   = {dash, dot, dash, dot, dash, dash};   
-// int periodSeq[]   = {dot, dash, dot, dash, dot, dash};    
-// int commaSeq[]    = {dash, dash, dot, dot, dash, dash};   
-// int semicolonSeq[]= {dash, dot, dash, dot, dash, dot};    
-// int colonSeq[]    = {dash, dash, dash, dot, dot, dot};    
-// int plusSeq[]     = {dot, dash, dot, dash, dot};          
-// int minusSeq[]    = {dash, dot, dot, dot, dot, dash};     
-// int slashSeq[]    = {dash, dot, dot, dash, dot};          
-// int equalsSeq[]   = {dash, dot, dot, dot, dash};
+int p = 0; //phrase counter
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
 class Sequence {
-    public: //For now, I'll make it private later (maybe)
-      int duration_sequence[20] = {0}; //20 is a place holder, I need to look at the morse alphabet to determine max number of inputs for a single char.
+    
+    public:
+
+      char charSequence[6] = {0};
       //Array is 0 initialized
       int size = 0;
-      
-    public:
+    
       Sequence() {}; //Default Constructor;
 
-      Sequence(int arr[], int n) { //parameter based constructor
-        size = n;
-        for (int i = 0; i < n; i++) {
-          duration_sequence[i] = arr[i];
-        }
+};
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+Sequence phrase[10];
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+class MorseTree {
+    private:
+      class Node {
+        public:
+          char data;  
+          Node* left;
+          Node* right;
+
+          Node(char c) { //parameter based constructor
+            this->data = c;
+            this->left = nullptr;
+            this->right = nullptr;
+          }
+
       };
+    
+    Node* root;
 
-      bool operator==(const Sequence& other) const {
-        if (this->size != other.size)
-          return false;
-        
-        for (int i = 0; i < other.size; i++) {
-          if (abs(other.duration_sequence[i] - this->duration_sequence[i]) <= 50)
-            continue;
+    public:
 
-          else
-            return false;
+      char decodeSequence(const Sequence& seq) {
+        Node* current = root;
+
+        for (int i = 0; i < seq.size; i++) {
+          if (seq.charSequence[i] == dot)
+            current = current->left;
+          else if (seq.charSequence[i] == dash)
+            current = current->right;
+
+          if (current == nullptr)
+            return '?';  // invalid sequence
         }
-        return true;
+
+        return current->data;
       }
 
+      MorseTree() {
+
+        root = new Node('\0');
+
+        // Level 1
+        root->left  = new Node('E');
+        root->right = new Node('T');
+
+        // Level 2
+        root->left->left  = new Node('I');
+        root->left->right = new Node('A');
+        root->right->left = new Node('N');
+        root->right->right= new Node('M');
+
+        // Level 3
+        root->left->left->left   = new Node('S');
+        root->left->left->right  = new Node('U');
+        root->left->right->left  = new Node('R');
+        root->left->right->right = new Node('W');
+        root->right->left->left  = new Node('D');
+        root->right->left->right = new Node('K');
+        root->right->right->left = new Node('G');
+        root->right->right->right= new Node('O');
+
+        // Level 4
+        root->left->left->left->left   = new Node('H');
+        root->left->left->left->right  = new Node('V');
+        root->left->left->right->left  = new Node('F');
+        root->left->left->right->right = new Node('\0');
+        root->left->right->left->left  = new Node('L');
+        root->left->right->left->right = new Node('\0');
+        root->left->right->right->left = new Node('P');
+        root->left->right->right->right= new Node('J');
+
+        root->right->left->left->left   = new Node('B');
+        root->right->left->left->right  = new Node('X');
+        root->right->left->right->left  = new Node('C');
+        root->right->left->right->right = new Node('Y');
+        root->right->right->left->left  = new Node('Z');
+        root->right->right->left->right = new Node('Q');
+        root->right->right->right->left = new Node('\0');
+        root->right->right->right->right= new Node('\0');
+
+        // Level 5 (numbers + some symbols)
+        root->left->left->left->left->left   = new Node('5');
+        root->left->left->left->left->right  = new Node('4');
+        root->left->left->left->right->right = new Node('3');
+        root->left->left->right->right->right= new Node('2');
+        root->left->right->right->right->right= new Node('1');
+
+        root->right->left->left->left->left  = new Node('6');
+        root->right->right->left->left->left = new Node('7');
+        root->right->right->right->left->left= new Node('8');
+        root->right->right->right->right->left= new Node('9');
+        root->right->right->right->right->right= new Node('0');
+
+        // Level 6 (punctuation)
+        root->left->left->right->right->left->left = new Node('?');
+        root->left->right->left->right->left->right= new Node('.');
+        root->right->left->right->left->right->left= new Node(';');
+        root->right->left->right->left->right->right= new Node('!');
+        root->right->right->left->left->right->right= new Node(',');
+        root->right->left->left->left->left->right  = new Node('-');
+        root->right->left->left->left->right        = new Node('=');
+        root->right->left->left->right->left        = new Node('/');
+        root->right->right->right->left->right      = new Node(':');
+        root->left->right->left->right->left        = new Node('+');
+
+      }
+
+
+
 };
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
-Sequence seqAlphabet[] = {
-    Sequence(aSeq, 2),  Sequence(bSeq, 4),  Sequence(cSeq, 4),
-    Sequence(dSeq, 3),  Sequence(eSeq, 1),  Sequence(fSeq, 4),
-    Sequence(gSeq, 3),  Sequence(hSeq, 4),  Sequence(iSeq, 2),
-    Sequence(jSeq, 4),  Sequence(kSeq, 3),  Sequence(lSeq, 4),
-    Sequence(mSeq, 2),  Sequence(nSeq, 2),  Sequence(oSeq, 3),
-    Sequence(pSeq, 4),  Sequence(qSeq, 4),  Sequence(rSeq, 3),
-    Sequence(sSeq, 3),  Sequence(tSeq, 1),  Sequence(uSeq, 3),
-    Sequence(vSeq, 4),  Sequence(wSeq, 3),  Sequence(xSeq, 4),
-    Sequence(ySeq, 4),  Sequence(zSeq, 4),
-
-    // Sequence(oneSeq, 5),   Sequence(twoSeq, 5),   Sequence(threeSeq, 5),
-    // Sequence(fourSeq, 5),  Sequence(fiveSeq, 5),  Sequence(sixSeq, 5),
-    // Sequence(sevenSeq, 5), Sequence(eightSeq, 5), Sequence(nineSeq, 5),
-    // Sequence(zeroSeq, 5),
-
-    // Sequence(questionSeq, 6), Sequence(exclamSeq, 6), Sequence(periodSeq, 6),
-    // Sequence(commaSeq, 6),    Sequence(semicolonSeq, 6), Sequence(colonSeq, 6),
-    // Sequence(plusSeq, 5),     Sequence(minusSeq, 6),  Sequence(slashSeq, 5),
-    // Sequence(equalsSeq, 5)
-};
-
-char charAlphabet[] = {
-  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-  'Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6',
-  '7','8','9','0','?','!','.',',',';',':','+','-','/','='
-};
-
-Sequence phrase[10]; 
-
-//--------------------------------------------------------------------------------------------------------------------------------
-
-
-// const UnorderedMap<Sequence, char> = {
-//   {}
-// };
-
-//Dictionary needs to translate a sequence of inputs into chars
-//These inputs consists of ints (durations)
-//One character will be represented by a single sequence
-//So we can store in an array of a reasonable max_size all of the durations.
-//Do we need an intermediate conversion step to just store dots and dashes?
+MorseTree alphabet;
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
@@ -175,18 +180,27 @@ Sequence Sequencer() {
       byte value = digitalRead(button);
 
       if (value != previous_value) {
+        
         if (value == HIGH) {
           pause_duration = millis() - pause_start;
           start_time = millis();
           digitalWrite(buzzer, HIGH);
-        } else {
+        } 
+        else {
             pause_start = millis();
             press_duration = millis() - start_time;
 
-            currentSequence.duration_sequence[i] = press_duration;
+            if (50 <= press_duration && press_duration <= 150)
+              currentSequence.charSequence[i] = dot;
+  
+            else if (450 <= press_duration && press_duration <= 550)
+              currentSequence.charSequence[i] = dash;
+
             i++;
+
             digitalWrite(buzzer, LOW);
 
+            //These are for debugging, we'll do a bit of front-end later.
             Serial.print("Duration: ");
             Serial.print(press_duration);
             Serial.print("ms");
@@ -198,26 +212,24 @@ Sequence Sequencer() {
 
     } while (pause_duration < 6000);
     
+    currentSequence.size = i;
+    
     return currentSequence;
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------
 
 void Decode() {
   String decoded_message = "";
 
-  for (int i = 0; i < 10; i++) {
-    Sequence sq = phrase[i];
-
-    for (int j = 0; j < 46; j++) {
-      if (sq == seqAlphabet[j])
-        decoded_message += charAlphabet[j];
-      else
-        continue;
-    }
-  }
+  for (int i = 0; i < p; i++)
+    decoded_message += alphabet.decodeSequence(phrase[i]);
 
   Serial.println(decoded_message);
-
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
 
 void setup() {
   String starter_message = "Morse Code Reader Output:";
@@ -229,7 +241,6 @@ void setup() {
 
   Serial.begin(9600);
 
-  int size = sizeof(charAlphabet) / sizeof(charAlphabet[0]); //I will remove it later, I just want to know, and I can't be bother to count XD
   Serial.println();
 
 }
@@ -239,15 +250,17 @@ void setup() {
 //If not print out a waiting message, and call the decoder
 //Do not take in any inputs while the decoder is running.
 
-int p = 0; //phrase counter
 
 void loop() {
   
+  pause_duration = 0;
   byte value = digitalRead(button);
 
   if (value != previous_value) {
-    phrase[p] = Sequencer();
-    p++;
+    if (p < 10) {
+      phrase[p] = Sequencer();
+      p++;  
+    }
   }
   else if (pause_duration >= 6000)
     Decode();
@@ -268,4 +281,6 @@ void loop() {
 
 //As a simplifying assumption I will begin by creating a sequence class for a single sentence/sequence, I will later on try to generalize the program for continuous use.
 
-//Well, program is too fat, I'm gonna have to trim it down, quite a bit.
+//Change of plans, after some reasearch, and conversation with a friend. I've learned that morse code was actually built to be decoded like a binary search tree
+//Using the right direction for dashes, and the left direction for dots.
+//Using this fact, I can ditch the memory heavy dictionary I currently use.
